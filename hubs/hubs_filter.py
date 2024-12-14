4cb0321166225bf2f496e091a4a17b53de179f88
Jude Lopez <judecl@bu.edu>
2024-12-14 18:19:01 -0500

    printing formatting

--- hubs/hubs_filter.py
+++ hubs/hubs_filter.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 def grab_hubs(newFileName):
+    print("running...")
+
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=chrome_options)
@@ -16,16 +18,17 @@ def grab_hubs(newFileName):
     wic = courses[1].find_element(By.ID, "wic")
 
     courses = (wic.find_elements(By.CLASS_NAME, "cf-course"))
-    counter = 1
 
-    for course in courses:
-        print(counter)
+    for i in range(len(courses)):
+        if i % 20 == 0: print(f'{i}/{len(courses)}')
+
+        course = courses[i]
         h3s = course.find_elements(By.TAG_NAME, "h3")
 
         courseString = ""
-        for i in range(len(h3s)):
+        for j in range(len(h3s)):
             elem = h3s[i]
-            if i == 0:
+            if j == 0:
                 courseString += elem.find_element(By.CLASS_NAME, "cf-course-college").get_attribute('innerHTML')
                 courseString += f' {elem.find_element(By.CLASS_NAME, "cf-course-dept").get_attribute('innerHTML')}'
                 courseString += f' {elem.find_element(By.CLASS_NAME, "cf-course-number").get_attribute('innerHTML')}'
@@ -38,12 +41,12 @@ def grab_hubs(newFileName):
 
         if "Global Citizenship and Intercultural Literacy" in courseString:
             outputFile.write(f'{courseString}\n')
-        counter += 1
 
+    print("finished!")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(f'Too many args: {len(sys.argv)} passed, 1 required')
+        print(f'Too many args: {len(sys.argv)-1} passed, 1 required')
         print("Call with: python hubs_filter <output_file>")
         sys.exit(1)
-    grab_hubs(sys.argv[1], sys.argv[2])
+    grab_hubs(sys.argv[1])
--- prereqs/find_prereqs.py
+++ prereqs/find_prereqs.py
@@ -2,7 +2,6 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
 
 def findBeg(str, txt):
     return str.index(txt)
@@ -11,6 +10,8 @@ def findEnd(str, txt):
     return findBeg(str, txt)+len(txt)
 
 def grab_details(file, newFileName):
+    print("running...")
+
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=chrome_options)
@@ -22,28 +23,25 @@ def grab_details(file, newFileName):
     newFile = open(f'{newFileName}.txt', "w")
 
     for i in range(len(fileTextArr)):
+        if i % 20 == 0: print(f'{i}/{len(fileTextArr)}')
+
         line = fileTextArr[i]
         try:
-            print(f'line {i} start: {line}')
             courseNum = line[0:findBeg(line, " ")]
             driver.get(f'https://www.bu.edu/academics/cas/courses/cas-cs-{courseNum}')
             content = driver.find_element(By.ID, "course-content").text
             preReqs = content[findEnd(content, "Undergraduate Prerequisites:")+1:findBeg(content, "-")-1]
             line += f' | {preReqs}\n'
             newFile.write(line)
-            print(f'line {i} done\n')
         except:
             line += f' | no data\n'
             newFile.write(line)
-            print(f'line {i} ERROR\n')
-    totalTime = time.time()-startTime
-    print(f'total running time: {totalTime}s')
-    print(f'average time per line: {totalTime/len(fileTextArr)}s')
+
+    print("finished!")
 
 if __name__ == "__main__":
-    startTime = time.time()
     if len(sys.argv) != 3:
-        print(f'Too many args: {len(sys.argv)} passed, 2 required')
+        print(f'Too many args: {len(sys.argv)-1} passed, 2 required')
         print("Call with: python find_prereqs <input_file> <output_file>")
         sys.exit(1)
     grab_details(sys.argv[1], sys.argv[2])
